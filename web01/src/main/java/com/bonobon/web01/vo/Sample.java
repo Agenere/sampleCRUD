a22187dfb1fe8872ee5ccc5812e293ee211824d8
Administrator <Administrator@smart20102>
2019-02-08 10:56:20 +0900

    [김동열]sample.java 생성 -샘플테이블을 위한 vo 생성

---
+++ web01/src/main/java/com/bonobon/web01/vo/Sample.java
@@ -0,0 +1,24 @@
+package com.bonobon.web01.vo;
+
+public class Sample {
+	private int sampleId; //smaple.sample_id
+	private String sampleName; //smaple.sample_name
+	
+	public int getSampleId() {
+		return sampleId;
+	}
+	public void setSampleId(int sampleId) {
+		this.sampleId = sampleId;
+	}
+	public String getSampleName() {
+		return sampleName;
+	}
+	public void setSampleName(String sampleName) {
+		this.sampleName = sampleName;
+	}
+	@Override
+	public String toString() {
+		return "Sample [sampleId=" + sampleId + ", sampleName=" + sampleName + "]";
+	}
+	
+}
